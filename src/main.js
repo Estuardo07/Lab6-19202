e300e63ae3ee999ed1c2f0ad63cbacc870817d85
Estuardo07 <xavihernand7@hotmail.com>
2024-03-05 22:57:47 -0600

    docker-compose and final fixes

--- src/main.js
+++ src/main.js
@@ -79,5 +79,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server listening at http://arpanetos.lol:${port}`)
+  console.log(`Server listening at http://127.0.0.1:${port}`)
 })
